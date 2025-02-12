3c75be923089fcfb39437f721be1c26336c7d7f5
Neal Uehara <ueharaneal@gmail.com>
2025-02-11 22:57:08 -0800

    new db

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -2,14 +2,23 @@ import Link from "next/link";
 import React from "react";
 import { ProfileLinks } from "./ProfileLinks";
 import DefaultLinks from "./DefaultLinks";
+import Logo from '@/../public/assets/images/common/Logo.png'
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav className="sticky top-0 z-50 h-14 border-b bg-background">
       <div className="container mx-auto flex h-full items-center justify-between">
-        <div className="flex flex-row gap-x-12">
+        <div className="flex flex-row gap-x-8">
           <h3 className="text-2xl font-semibold tracking-tight">
-            <Link href="/">Carefinder </Link>
+              <div className="flex flex-row gap-x-4 items-center">
+            <Link href="/"> 
+             <Image src={Logo} alt="logo" className="object-contain w-7"/>
+              </Link>
+              <Link href="/"> 
+             <p>Carefinder</p> 
+             </Link>
+              </div>
           </h3>
           <DefaultLinks />
         </div>
