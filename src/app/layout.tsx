818d989cf11a18cb4852a0951014599989b5ce68
Neal Uehara <ueharaneal@gmail.com>
2025-02-25 10:23:58 -0800

    fixed color mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout({
             <HydrateClient>
               <ThemeProvider
                 attribute="class"
-                defaultTheme="dark"
-                enableSystem
+                defaultTheme="light"
                 disableTransitionOnChange
               >
                 <Script
